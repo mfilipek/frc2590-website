31bda0562a6c62d50c0c268e368f0f5e920346fa
Alex Wolfe <alexkimiwolfe@gmail.com>
2013-11-06 20:45:06 -0500

    adding logos
    For Pencils

---
+++ header.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+	Header File	
+	Markup for Header Universal to all pages
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<!-- metadata -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,300' rel='stylesheet' type='text/css'>
+	<link rel="icon" href="images/favicon.ico" />
+	<title>frc2590 | Team Nemesis Robotics | Robbinsville High School Robotics</title>
+	
+	<meta charset="utf-8">
+	<meta name="description" content= "FRC Team 2590, Nemesis, is an award winning FIRST Robotics team 
+		based out of Robbinsville High School in New Jersey">
+	<meta name="keywords" content= "nemesis,robotics,robbinsville,FIRST,frc2590,high school,competition">
+	
+	<!-- stylesheets -->
+	<link rel="stylesheet" href="scripts/FlexSlider/flexslider.css" type="text/css">
+	<link rel="stylesheet" href="css/main.css" type="text/css">
+	<!-- javascript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+	<script src="scripts/FlexSlider/jquery.flexslider-min.js"></script>
+
+</head>
+<body>
+	<header id="header-home" class="container">
+		<div id="nemesis">
+			<img src="images/Nemesis_transparent_black.png">
+			<div id="frc"><div class="red">//</div> FRC 2590</div>
+		</div>
+		<div id="logo"><a href= "index.html"><img src="images/logo_winged.jpg"></a></div>
+	</header>
+	
---
+++ processWire templates/home.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+	Index Page
+*/
+
+include("./header.php");
+?>
+<div id="main" class="container">
+
+<?php
+include("./footer.php");
+?>
